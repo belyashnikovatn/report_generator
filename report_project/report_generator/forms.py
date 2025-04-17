63f73e7bd70d3ce4b1d4ff8532f8aa476797854c
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2025-04-17 10:51:07 +0500

    fix: change logic + format

--- report_project/report_generator/forms.py
+++ report_project/report_generator/forms.py
@@ -2,7 +2,4 @@ from django import forms
 
 
 class CSVUploadForm(forms.Form):
-    csv_file = forms.FileField(
-        label='Выберите CSV файл',
-        widget=forms.FileInput(attrs={'accept': '.csv'})
-    )
+    file = forms.FileField(label='Загрузите CSV файл')
--- report_project/report_project/urls.py
+++ report_project/report_project/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from report_generator import views
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.upload_csv, name='upload_csv'),
+    path('', include('report_generator.urls')),
 ]
