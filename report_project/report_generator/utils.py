6951a2700b8d6b315440dfd76590ec090a49b326
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2025-04-17 12:00:36 +0500

    refactor

--- report_project/report_generator/utils.py
+++ report_project/report_generator/utils.py
@@ -1,5 +1,4 @@
 from docx import Document
-from docx.shared import Inches
 from django.conf import settings
 
 
@@ -11,7 +10,8 @@ def generate_docx_report(data, total):
 
     # Заголовок
     doc.add_heading('1.6.1. Автомобильный транспорт', level=1)
-    doc.add_heading('1.6.1.1. Автомобильные дороги федерального, регионального и межмуниципального значения', level=2)
+    doc.add_heading(
+        '1.6.1.1. Автомобильные дороги федерального, регионального и межмуниципального значения', level=2)
 
     doc.add_paragraph(
         'Внешние транспортные связи населенных пунктов обеспечиваются автомобильным транспортом, '
@@ -21,7 +21,8 @@ def generate_docx_report(data, total):
         'На территории муниципального округа не расположены автомобильные дороги федерального значения.'
     )
 
-    doc.add_paragraph('Таблица 1.6.1.1.1 Дороги общего пользования регионального и межмуниципального значения:')
+    doc.add_paragraph(
+        'Таблица 1.6.1.1.1 Дороги общего пользования регионального и межмуниципального значения:')
 
     # Таблица
     table = doc.add_table(rows=1, cols=len(display_fields) + 1)
@@ -31,14 +32,22 @@ def generate_docx_report(data, total):
     for i, field in enumerate(display_fields):
         hdr_cells[i + 1].text = field
 
+    # Заполнение таблицы данными
     for idx, row in enumerate(data, start=1):
         row_cells = table.add_row().cells
         row_cells[0].text = str(idx)
         for i, field in enumerate(display_fields):
             row_cells[i + 1].text = str(row.get(field, ''))
 
-    # Сумма
-    doc.add_paragraph(f"\nИтого: {total}")
+    # Итоговая строка с суммой под нужным столбцом
+    last_column_index = len(display_fields)
+    row_cells = table.add_row().cells
+    row_cells[0].text = 'Итого:'
+    for i in range(1, len(display_fields)):
+        row_cells[i].text = ''
+    row_cells[last_column_index].text = str(total)
+
+    # Подпись под итогом
     doc.add_paragraph(
         f"Протяженность автомобильных дорог регионального или межмуниципального значения составляет {total} км."
     )
